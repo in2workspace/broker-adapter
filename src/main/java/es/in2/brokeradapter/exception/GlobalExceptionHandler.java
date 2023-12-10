b1de78cb0269b72732be71b30b7f5ea50967a778
Oriol Canadés <oriol.canades@in2.es>
2023-12-10 15:27:30 +0100

    Update GitHub Actions

--- src/main/java/es/in2/brokeradapter/exception/GlobalExceptionHandler.java
+++ src/main/java/es/in2/brokeradapter/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package es.in2.brokeradapter.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Mono;
 
 import java.util.NoSuchElementException;
@@ -15,35 +17,35 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public Mono<Void> handleEntityNotFoundException(NoSuchElementException e) {
+    public Mono<Void> handleEntityNotFoundException() {
         return Mono.empty();
     }
 
     @ExceptionHandler(EntityAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     @ResponseBody
-    public Mono<Void> handleEntityAlreadyExistException(EntityAlreadyExistException e) {
+    public Mono<Void> handleEntityAlreadyExistException() {
         return Mono.empty();
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public Mono<Void> handleEntityUnauthorizedAccessException(UnauthorizedAccessException e) {
+    public Mono<Void> handleEntityUnauthorizedAccessException() {
         return Mono.empty();
     }
 
     @ExceptionHandler(ForbiddenAccessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public Mono<Void> handleEntityForbiddenAccessException(ForbiddenAccessException e) {
+    public Mono<Void> handleEntityForbiddenAccessException() {
         return Mono.empty();
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public Mono<Void> handleEntityRuntimeException(RuntimeException e) {
+    public Mono<Void> handleEntityRuntimeException() {
         return Mono.empty();
     }
 
