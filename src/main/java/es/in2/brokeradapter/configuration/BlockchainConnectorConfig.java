ed6b49cd63b574d9d50ac9b2229b72307f523c50
yaniaiciin2 <yani.aici@in2.es>
2024-01-05 10:18:12 +0100

    Broker Adapter now makes Blockchain Connector Subscriptions
    ADD:
    New BlockchainConnectorConfig class and new NgsiLdProperties

---
+++ src/main/java/es/in2/brokeradapter/configuration/BlockchainConnectorConfig.java
@@ -0,0 +1,43 @@
+package es.in2.brokeradapter.configuration;
+
+import es.in2.brokeradapter.configuration.properties.NgsiLdSubscriptionConfigProperties;
+import es.in2.brokeradapter.domain.SubscriptionRequest;
+import es.in2.brokeradapter.service.SubscriptionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class BlockchainConnectorConfig {
+    private final NgsiLdSubscriptionConfigProperties subscriptionConfiguration;
+    private final SubscriptionService subscriptionService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public Mono<Void> setBrokerSubscription() {
+        String processId = UUID.randomUUID().toString();
+        log.info("ProcessID: {} - Setting Orion-LD Entities subscription...", processId);
+        SubscriptionRequest brokerSubscriptionRequest = SubscriptionRequest.builder()
+                .id("urn:ngsi-ld:Subscription:" + UUID.randomUUID())
+                .type("Subscription")
+                .notificationEndpointUri(subscriptionConfiguration.notificationEndpoint())
+                .entities(subscriptionConfiguration.entityTypes())
+                .build();
+
+        log.debug("ProcessID: {} - Broker Subscription: {}", processId, brokerSubscriptionRequest.toString());
+        return subscriptionService.createSubscription(processId, brokerSubscriptionRequest)
+                .doOnError(e -> {
+                    log.error("ProcessID: {} - Error creating default subscription", processId, e);
+                    if (e instanceof InterruptedException) {
+                        Thread.currentThread().interrupt();
+                    }
+                })
+                .doOnSuccess(response -> log.info("ProcessID: {} - Default subscription created successfully", processId));
+    }
+}
